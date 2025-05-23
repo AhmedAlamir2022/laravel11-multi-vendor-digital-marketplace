64efabc42088be273846e52848dd79f52bbe2a93
Ahmed Alamir <ahmedalamir521@gmail.com>
2025-05-23 17:41:11 +0300

    admin-panel-setup

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta20
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Admin Dashboard </title>
+    <!-- CSS files -->
+    <link href="{{ asset('assets/admin/css/tabler.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/tabler-flags.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/tabler-payments.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/demo.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/bootstrap-tagsinput.css') }}" rel="stylesheet" />
+
+    @stack('styles')
+
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
+
+</head>
+
+<body>
+    <script src="{{ asset('assets/admin/js/demo-theme.min.js') }}"></script>
+    <div class="page">
+        <!-- Sidebar -->
+        @include('admin.layouts.sidebar')
+
+        @yield('content')
+
+    </div>
+
+    <!-- Libs JS -->
+    <script src="{{ asset('assets/admin/js/jquery.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+    <script src="{{ asset('assets/admin/js/bootstrap-tagsinput.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/select2.min.js') }}"></script>
+
+    <!-- Tabler Core -->
+    <script src="{{ asset('assets/admin/js/tabler.min.js') }}" defer></script>
+    <script src="{{ asset('assets/admin/js/demo.min.js') }}" defer></script>
+    <!-- TinyMce  -->
+    <script src="{{ asset('assets/frontend/js/tinymce/tinymce.min.js') }}"></script>
+
+    <!-- Admin JS -->
+    <script src="{{ asset('assets/admin/js/default/admin.js') }}"></script>
+
+    @stack('scripts')
+</body>
+
+</html>
