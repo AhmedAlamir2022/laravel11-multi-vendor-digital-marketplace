4f71a8f80db207e917567b1cebdac96ae1b088c0
Ahmed Alamir <ahmedalamir521@gmail.com>
2025-05-25 14:54:28 +0300

    kyc_options

---
+++ app/Http/Controllers/Admin/KYCSettingController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\KycSettingUpdateRequest;
+use App\Models\KycSetting;
+use App\Services\NotificationService;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class KYCSettingController extends Controller
+{
+    function index() : View
+    {
+        $kycSetting = KycSetting::first();
+        return view('admin.kyc.kyc-setting.index', compact('kycSetting'));
+    }
+
+    function update(KycSettingUpdateRequest $request) : RedirectResponse
+    {
+        KycSetting::updateOrCreate(
+            ['id' => 1],
+            $request->validated()
+        );
+
+        NotificationService::UPDATED();
+        return redirect()->back();
+    }
+}
